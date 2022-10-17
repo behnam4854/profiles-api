fda0a95183022b4171092dc6e68742d358a6b9fb
ibehnam <behnam4854@gmail.com>
2022-10-17 21:33:56 +0200

    api view added

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """test for seeing api views"""
+    def get(self, request, format=None):
+        """return some things """
+        return Response("yay you have done what you use to do before")
