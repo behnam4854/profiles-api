a0795b18a744a27b330fa4f7e289b5e0aa755b9b
ibehnam <behnam4854@gmail.com>
2022-10-18 11:12:27 +0200

    added views to api and authentication system with modheader

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -6,6 +6,8 @@ from profiles_api import serializers
 from profiles_api import models
 from rest_framework.authentication import TokenAuthentication
 from . import permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 
 class HelloApiView(APIView):
@@ -22,3 +24,8 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """for handling user authenthication token"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
